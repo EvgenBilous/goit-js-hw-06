d10a1cd48d8f5e6cb51a93b1068605520c43b69c
Eugene <131405736+EvgenBilous@users.noreply.github.com>
2023-09-03 20:43:46 -0500

    fix initial fontsize

--- js/task-07.js
+++ js/task-07.js
@@ -1,6 +1,6 @@
 const inputEl = document.querySelector("#font-size-control");
 const spanEle = document.querySelector("#text");
-
+spanEle.style.fontSize = "16px";
 inputEl.addEventListener("input", () => {
   spanEle.style.fontSize = `${inputEl.value}px`;
 });
